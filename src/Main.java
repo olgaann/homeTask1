f1be3c33cfeb73d02afe87d3f5cb712175485810
olga <bochkareva-o.a@yandex.ru>
2024-03-13 10:11:46 +0300

    selectionSort added

--- src/Main.java
+++ src/Main.java
@@ -18,4 +18,24 @@ public class Main {
         }
         System.out.println(Arrays.toString(arr));;
     }
+
+
+    public static void selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int indexMin = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[indexMin]) {   //чтобы сортировать по убыванию просто меняй > на <, а indexMin на indexMax
+                    indexMin = j;
+                }
+            }
+            if (i != indexMin) {
+                int temp = arr[i];
+                arr[i] = arr[indexMin];
+                arr[indexMin] = temp;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
 }
