f38eacdc17d63dee784b9d84f08d0fafb0d3f426
olga <bochkareva-o.a@yandex.ru>
2024-03-13 11:16:13 +0300

    insertionSort added

--- src/Main.java
+++ src/Main.java
@@ -38,4 +38,19 @@ public class Main {
 
         System.out.println(Arrays.toString(arr));
     }
+
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            // Перемещаем элементы массива, которые больше key, на одну позицию вперед
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
 }
